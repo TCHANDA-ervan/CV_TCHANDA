194297c2da94794642a6a814453c137566f5bbae
TCHANDA-ervan <94933948+TCHANDA-ervan@users.noreply.github.com>
2021-12-04 08:48:52 +0100

    Add files via upload

---
+++ partie_gauche2.php
@@ -0,0 +1,31 @@
+<div id="partie_gauche2">
+                        
+                        <div id="gat">
+                            <p id="i1"><img src="image/cake.png" style="width: 40px; height:40px " alt="gateau d'anniversaire"></p> 
+                            <p id="text1"><?php echo $naissance['naissance']?>
+                                <br><?php echo $origine['origine']?>
+                                <br><?php echo $matrimoniale['matrimoniale']?></p>
+                        </div>
+                             
+                            <hr id="ligne10">
+                            <p id="i2"><img src="image/local.png" style="width: 35px; height:35px" alt="Localisation"></p>
+                            <p id="text2"><?php echo $quartier['quartier']?>
+                                <br><?php echo $VillePays['VillePays']?>
+                                <br><?php echo $map['map']?></p>
+                            <hr id="ligne11">
+
+                           <p id="i3"><img src="image/phone.png" style="width: 35px; height:35px" alt="Localisation"></p>
+                           <p id="text3"><?php echo $tel['tel']?>
+                               <br><?php echo $reseau['reseau']?></p>
+                           <hr id="ligne12">
+                           
+                           <p id="i4"><img src="image/envelope.png" style="width: 35px; height:35px" alt="Localisation"></p>
+                           <p id="text4"><?php echo $mail['mail']?>
+                               <br><?php echo $lien['lien']?></p>
+                             <br>
+                           <p id="text5">
+                          <tia style="color:rgba(255, 255, 255, 0.856);">+100 PROJETS</tia> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; +20 CONTACTS&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                              12 ANS D'EXP
+                             </p>
+            <hr id="ligne13">
+                    </div>
---
+++ partie_gauche3.php
@@ -0,0 +1,44 @@
+<div id="partie_gauche3">
+                        <div id="get_text9">
+                            <p id="check"><input style="background: blue;" type="checkbox" checked>&nbsp;&nbsp;&nbsp;&nbsp; <strong><?php echo $DEVF['DEVF']?></strong></p>
+                            <p id="spa"><span><img src="image/star.png" alt="etoile"></span></p>
+                        </div>
+                        <p id="sub1"><sub><?php echo $formation1['formation1']?></sub></p>
+                        <p id="pro1"><input style="width: 283px;" type="range" max=100 value=80></p>
+        
+                        <div id="get_text10">
+                            <p id="check"><input type="checkbox" checked>&nbsp;&nbsp;&nbsp;&nbsp; <strong><?php echo $DEVB['DEVB']?></strong></p>
+                            <p id="spa"><span><img src="image/star.png" alt="etoile"></span></p>
+                        </div>
+                        <p id="sub1"><sub><?php echo $formation2['formation2']?></sub></p>
+                        <p id="pro1"><input style="width: 283px;" type="range" max=100 value=30></p>
+        
+                        <div id="get_text10">
+                            <p id="check"><input type="checkbox" checked >&nbsp;&nbsp;&nbsp;&nbsp; <strong><?php echo $DEVM['DEVM']?></strong></p>
+                            <p id="spa"><span><img src="image/star.png" alt="etoile"></span></p>
+                        </div>
+                        <p id="sub1"><sub><?php echo $formation3['formation3']?></sub></p>
+                        <p id="pro1"><input style="width: 283px;" type="range" max=100 value=10></p>
+        
+                        <div id="get_text10">
+                            <p id="check"><input type="checkbox" checked>&nbsp;&nbsp;&nbsp;&nbsp; <strong><?php echo $UI['UI']?></strong></p>
+                            <p id="spa"><span><img src="image/star.png" alt="etoile"></span></p>
+                        </div>
+                        <p id="sub1"><sub><?php echo $formation4['formation4']?></sub></p>
+                        <p id="pro1"><input style="width: 283px;" type="range" max=100 value=15></p>
+        
+                        <div id="get_text10">
+                            <p id="check"><input type="checkbox" checked>&nbsp;&nbsp;&nbsp;&nbsp; <strong><?php echo $BD['BD']?></strong></p>
+                            <p id="spa"><span><img src="image/star.png" alt="etoile"></span></p>
+                        </div>
+                        <p id="sub1"><sub><?php echo $formation5['formation5']?></sub></p>
+                        <p id="pro1"><input style="width: 283px;" type="range" max=100 value=20></p>
+        
+                        <div id="get_text10">
+                            <p id="check"><input type="checkbox" checked>&nbsp;&nbsp;&nbsp;&nbsp; <strong><?php echo $OUTIL['OUTIL']?> </strong></p>
+                            <p id="spa"><span><img src="image/star.png" alt="etoile"></span></p>
+                        </div>
+                        <p id="sub1"><sub><?php echo $formation6['formation6']?></sub></p>
+                        <p id="pro1"><input style="width: 283px;" type="range" max=100 value=30></p>
+                    </div>
+                    </div>
